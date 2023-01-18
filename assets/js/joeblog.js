b8f086743ee7f0464e4e8a1f155b9249a2559a03
Lovi Stark <lovishb41@gmail.com>
2023-01-18 15:27:33 +0530

    zindex

--- assets/js/joeblog.js
+++ assets/js/joeblog.js
@@ -54,7 +54,9 @@ var canvas3Settings = {
     colors: [[165,104,246],[230,61,135],[0,199,228],[253,214,126]],
     clock: 15
   };
-  var canvas = new ConfettiGenerator(canvas3Settings);
+var canvas = new ConfettiGenerator(canvas3Settings);
+var canvasz = document.getElementById('my-canvas')
+
 
 var confettiSettings = { target: 'my-canvas' };
         const startit = () => {
@@ -70,7 +72,9 @@ var confettiSettings = { target: 'my-canvas' };
       console.log("stop");
       canvas.clear();
     }, 6000);
+    canvasz.style.zIndex = "-1";
   };
+
 
   startit();
   stopit();
